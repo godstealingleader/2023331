70672912c2f83facb31b9d4f41f8021a8047a10d
godstealingleader <107918254+godstealingleader@users.noreply.github.com>
2023-03-31 11:11:44 +0800

    Add files via upload

---
+++ 2023331.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	int n_long = 0;
+	cout << "Please enter the converted quantity" << endl;
+	cin >> n_long;
+	cout << n_long << "long is " << n_long * 220 << "yd" << endl;
+	return 0;
+}
